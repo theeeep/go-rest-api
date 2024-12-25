6e8494bc672f790da96fae5377b9ceda30ad455a
Deepak <ideepakrabindra@gmail.com>
2024-12-25 18:52:02 +0530

    Fix server start issues

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/theeeep/go-rest-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Load Config
+	cfg := config.MustLoad()
+
+	// db setup
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("🌐 Server started on %s\n", cfg.HTTPServer.Addr)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
 }
