6b94af01ceb4fdb2f717b72f5b6526fbbf46f1aa
Deepak <ideepakrabindra@gmail.com>
2024-12-27 21:52:42 +0530

    Gracefull shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/theeeep/go-rest-api/internal/config"
 )
@@ -28,14 +33,30 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Printf("🌐 Server started on %s\n", cfg.HTTPServer.Addr)
+	slog.Info("Starting server", "addr", fmt.Sprintf("http://%s", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		err := server.ListenAndServe()
 
 		if err != nil {
-			log.Fatalf("Failed to start server: %v", err)
+			slog.Error("Failed to start server: ", slog.String("error", err.Error()))
 		}
 	}() // () --> anonymous function means it will be executed immediately
 
+	<-done
+
+	slog.Info("Shutting down server...") // slog --> structured logging
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // context(ctx) --> allows us to cancel operations
+	defer cancel()                                                          // defer --> executes a function at the end
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server: ", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server exited properly")
 }
